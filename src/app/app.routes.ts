869cfdc4693cd0a75a45d21e0ece744987ee9341
otavioapc <otavioaugcampos08@gmail.com>
2026-06-16 16:32:21 -0300

    feat: adicao do mapeamento de rotas do site

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/login/login.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { ConsultasComponent } from './features/consultas/consultas.component';
+import { PacientesComponent } from './features/pacientes/pacientes.component';
+import { DentistasComponent } from './features/dentistas/dentistas.component';
+import { EspecialidadesComponent } from './features/especialidades/especialidades.component';
+import { RelatoriosComponent } from './features/relatorios/relatorios.component';
+import { UsuariosComponent } from './features/usuarios/usuarios.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'usuarios', component: UsuariosComponent },
+  { path: 'consultas', component: ConsultasComponent },
+  { path: 'pacientes', component: PacientesComponent },
+  { path: 'dentistas', component: DentistasComponent },
+  { path: 'especialidades', component: EspecialidadesComponent },
+  { path: 'relatorios', component: RelatoriosComponent },
+  { path: '**', redirectTo: 'login' }
+];
