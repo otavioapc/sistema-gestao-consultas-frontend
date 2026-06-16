a71a6662cc634d9c02086fb488934b5949d52eee
otavioapc <otavioaugcampos08@gmail.com>
2026-06-16 18:19:42 -0300

    feat: adicao do html da pagina de dashboard

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
-
+  today: Date = new Date();
 }
