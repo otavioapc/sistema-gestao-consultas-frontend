fe1d01ce76a8aecb99b30e26f5e7a2272145a9c3
otavioapc <otavioaugcampos08@gmail.com>
2026-06-16 18:08:00 -0300

    feat: adicao de condicao se usuario esta na tela de login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'vestaplan';
+  mostrarMenu: boolean = false;
+
+  constructor(private readonly router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      this.mostrarMenu = !(event.url === '/login' || event.url === '/' || event.url.includes('login'));
+    });
+  }
+
+  logout(): void {
+    localStorage.removeItem('vestaplan_token');
+    this.router.navigate(['/login']);
+  } 
 }
