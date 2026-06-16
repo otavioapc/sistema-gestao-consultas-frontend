98b2641050f94d2608a5cb7b29a2e414ec8f1723
otavioapc <otavioaugcampos08@gmail.com>
2026-06-16 16:58:24 -0300

    fix: correcao de compilacao do login.component.ts

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -6,13 +6,6 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
-})
-export class LoginComponent {
-  selector: 'app-login',
-  standalone: true,
   imports: [CommonModule, FormsModule, ReactiveFormsModule], 
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
@@ -26,20 +19,18 @@ export class LoginComponent {
     private readonly fb: FormBuilder,
     private readonly router: Router
   ) {
-
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       senha: ['', [Validators.required, Validators.minLength(4)]]
     });
   }
 
-  // Método que o botão "Entrar" vai disparar
   onSubmit(): void {
     if (this.loginForm.valid) {
       const dadosLogin = this.loginForm.value;
       console.log('Dados preenchidos no formulário:', dadosLogin);
       
-      // TODO: Conectar com o back-end nos próximos passos
+  
       this.router.navigate(['/dashboard']);
     } else {
       this.mensagemErro = 'Por favor, preencha todos os campos corretamente.';
