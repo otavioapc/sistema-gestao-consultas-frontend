8c7bf0b5ff4130d358a6ad30292a803b8177aa84
otavioapc <otavioaugcampos08@gmail.com>
2026-06-16 16:20:00 -0300

    feat: criacao dos components de login, dashboard, usuarios, consultas, pacientes, dentistas, especialidades, relatorios

---
+++ src/app/features/dentistas/dentistas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dentistas',
+  imports: [],
+  templateUrl: './dentistas.component.html',
+  styleUrl: './dentistas.component.scss'
+})
+export class DentistasComponent {
+
+}
---
+++ src/app/features/especialidades/especialidades.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-especialidades',
+  imports: [],
+  templateUrl: './especialidades.component.html',
+  styleUrl: './especialidades.component.scss'
+})
+export class EspecialidadesComponent {
+
+}
